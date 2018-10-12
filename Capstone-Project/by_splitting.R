fc516fdce948b08202a1cf603a56af9b877bf79e
Arind Mishra <mishra.arind@gmail.com>
2018-10-12 17:04:44 +0000

    optimized

--- Capstone-Project/by_splitting.R
+++
@@ -1,3 +0,0 @@
-files <- c("en_US.twitter.txt", "en_US.news.txt", "en_US.blogs.txt")
-
-files.prt <- file.split("../en_US.blogs.txt", size = 50000, same.dir = FALSE, verbose = TRUE, suf = "part", win = TRUE)
